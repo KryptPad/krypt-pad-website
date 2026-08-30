8a1f809d730aa3ac1324b5137e780ea7864841cc
neptunecentury <e.c.butler@outlook.com>
2026-08-30 01:39:13 -0400

    website styles and download page updates

--- src/routes/DownloadView.vue
+++ src/routes/DownloadView.vue
@@ -1,6 +1,24 @@
 <template>
     <div>
-        <h2>Download page coming soon!</h2>
+        <h1>Download Krypt Pad</h1>
+
+        <p>
+            Download Krypt Pad for your platform. Krypt Pad is available for Windows, Mac, and
+            Linux. Choose the appropriate version for your operating system.
+        </p>
+        <div class="d-flex justify-content-around">
+            <div class="card card-interactive m-2 text-center" tabindex="0">
+                Download for Windows
+            </div>
+
+            <div class="card card-interactive m-2 text-center" tabindex="0">
+                Download for Mac
+            </div>
+
+            <div class="card card-interactive m-2 text-center" tabindex="0">
+                Download for Linux (Debian/Ubuntu)
+            </div>
+        </div>
     </div>
 </template>
 
